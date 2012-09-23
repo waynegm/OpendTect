67b94707732039712724f7968b08699a88d1fe9f
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-09-23 21:50:37 +0000

    Add from trunk.
    git-svn-id: https://svn.opendtect.org/od/branches/od_osg@26342 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ CMakeModules/ODSubversion.cmake
@@ -0,0 +1,43 @@
+#_______________________Pmake__________________________________________________
+#
+#	CopyRight:	dGB Beheer B.V.
+# 	Jan 2012	K. Tingdahl
+#	RCS :		$Id$
+#_______________________________________________________________________________
+
+
+# the FindSubversion.cmake module is part of the standard distribution
+if ( WIN32 )
+    set ( CMAKE_SYSTEM_PROGRAM_PATH ${CMAKE_SYSTEM_PROGRAM_PATH}
+	    "C:/Program Files/SlikSvn/bin"
+	    "C:/Program Files (x86)/SlikSvn/bin" )
+    #Add more likely paths if need be
+endif()
+
+include(FindSubversion)
+
+# extract working copy information for SOURCE_DIR into MY_XXX variables
+if ( Subversion_FOUND )
+    Subversion_WC_INFO( ${CMAKE_SOURCE_DIR} MY)
+else()
+    set ( MY_WC_REVISION 0 )
+    set ( MY_WC_URL "" )
+endif()
+
+set ( INC_DIR ${CMAKE_SOURCE_DIR}/include/Basic )
+set ( TMPFILE ${INC_DIR}/svnversion.h.tmp )
+
+# write a file with the SVNVERSION define
+file( WRITE ${TMPFILE} 
+     "// Generated automatically by CMakeModules/ODSubversion.cmake\n"
+     "//\n"
+     "#ifndef mSVN_VERSION\n"
+     "#define mSVN_VERSION ${MY_WC_REVISION}\n"
+     "#define mSVN_URL \"${MY_WC_URL}\"\n"
+     "#endif\n")
+
+# copy the file to the final header only if the version changes
+# reduces needless rebuilds
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                        ${TMPFILE} ${INC_DIR}/svnversion.h )
+file ( REMOVE ${TMPFILE} )

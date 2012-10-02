57fa71d1130b09c543445588059f7727d1c70023
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-10-02 06:35:37 +0000

    Added macro for remans, and implementation for RefMan and OsgRefMan.
    git-svn-id: https://svn.opendtect.org/od/branches/od_osg@26559 a985855d-d384-4b45-ae33-31a410998c3c

--- include/visBase/visosg.h
+++ include/visBase/visosg.h
@@ -15,10 +15,14 @@ ________________________________________________________________________
 
 /*! Definition of macros used to make osg-life easier */
 
+#include "refcount.h"
+
 #define mGetOsgArrPtr(tp,ptr) ((tp) ptr->getDataPointer() )
 #define mGetOsgVec2Arr(ptr) ((osg::Vec2Array*) ptr )
 #define mGetOsgVec3Arr(ptr) ((osg::Vec3Array*) ptr )
 #define mGetOsgVec4Arr(ptr) ((osg::Vec4Array*) ptr )
+
+mDefRefMan( OsgRefMan, ref, unref )
 
 #endif
 

09282bee1f318c72273dd458d9ea18a01fc87e6c
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-10-16 15:32:13 +0000

    Initial add.
    git-svn-id: https://svn.opendtect.org/od/branches/od_osg@26810 a985855d-d384-4b45-ae33-31a410998c3c

---
+++ include/visBase/visnodestate.h
@@ -0,0 +1,48 @@
+#ifndef visnodestate_h
+#define visnodestate_h
+
+/*+
+________________________________________________________________________
+
+ (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+ Author:	Kristofer Tingdahl
+ Date:		Oct 2012
+ RCS:		$Id: viscoord.h 26346 2012-09-24 05:48:51Z kristofer.tingdahl@dgbes.com $
+________________________________________________________________________
+
+
+-*/
+
+#include "visosg.h"
+#include "objectset.h"
+
+namespace osg { class StateSet; class StateAttribute; }
+
+namespace visBase
+{
+    
+/*!Baseclass for objects manipulating the osg::StateSet. */
+
+class NodeState
+{ mRefCountImpl(NodeState);
+public:
+
+    void			setStateSet(osg::StateSet*);
+
+protected:
+				NodeState();
+    
+    template <class T> T*	addAttribute(T* a) { doAdd(a); return a; }
+
+    
+    ObjectSet<osg::StateAttribute>	attributes_;
+    
+private:
+    void				doAdd(osg::StateAttribute*);
+    OsgRefMan<osg::StateSet>		stateset_;
+};
+
+};
+
+#endif
+

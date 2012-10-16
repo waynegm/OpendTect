5e6140ae2771253f06a28646ff3b4b0732d03308
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-10-16 11:59:03 +0000

    Added Values section. We'll see if this is enough.
    git-svn-id: https://svn.opendtect.org/od/branches/od_osg@26794 a985855d-d384-4b45-ae33-31a410998c3c

--- include/visBase/visosg.h
+++ include/visBase/visosg.h
@@ -44,6 +44,7 @@ mDefRefMan( OsgRefMan, refOsgObj(ptr_), unrefOsgObj(ptr_) )
 #if defined(visBase_EXPORTS) || defined(VISBASE_EXPORTS)
 //Only available in visBase
 #include <osg/Vec3>
+#include <osg/Vec3d>
 #include <position.h>
 #include <convert.h>
     
@@ -56,7 +57,34 @@ namespace Conv
     template <>
     inline void set( osg::Vec3f& _to, const Coord3& v )
     { _to.set( (float) v.x, (float) v.y, (float) v.z ); }
+    
+    template <>
+    inline void set( Coord3& _to, const osg::Vec3d& v )
+    { _to.x = v[0]; _to.y=v[1]; _to.z=v[2]; }
+    
+    template <>
+    inline void set( osg::Vec3d& _to, const Coord3& v )
+    { _to.set(  v.x, v.y, v.z ); }
+    
 } //Namespace conv
+
+namespace Values
+{
+    template<>
+    class Undef<osg::Vec3f>
+    {
+    public:
+	static void		setUdf( osg::Vec3f& i )	{}
+    };
+    
+    template<>
+    class Undef<osg::Vec3d>
+    {
+    public:
+	static void		setUdf( osg::Vec3d& i )	{}
+    };
+
+} //Namespace Values
 
 #endif
 

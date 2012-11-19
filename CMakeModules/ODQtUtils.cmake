5dcc6745f35e2d4ee7fcbb337bf55662f40ad5d9
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-11-19 22:55:08 +0000

    Fixed typo.
    git-svn-id: https://svn.opendtect.org/od/branches/od_qt@27553 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODQtUtils.cmake
+++ CMakeModules/ODQtUtils.cmake
@@ -14,8 +14,8 @@ MACRO(OD_SETUP_QT)
 
     #Try to find Qt5
     list ( APPEND CMAKE_PREFIX_PATH ${QTDIR} )
-    find_package( Qt5Widgets )
-    if ( Qt5Widgets_FOUND QUIET )
+    find_package( Qt5Widgets QUIET )
+    if ( Qt5Widgets_FOUND )
 	cmake_minimum_required( VERSION 2.8.9 )
 	set( CMAKE_AUTOMOC ON )
     else()

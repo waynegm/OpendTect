11fffc9028b7cf8aa58d61c106a6256727022fd5
Ding Zheng <ding.zheng@dgbes.com>
2012-12-07 09:09:14 +0000

    Made QT use *LIBRARY variable (rather than *LIBRARY_RELEASE) as input library. This will make cmake select debug in debug mode, and release in release mode.
    git-svn-id: https://svn.opendtect.org/od/branches/od_osg@27805 a985855d-d384-4b45-ae33-31a410998c3c

--- CMakeModules/ODZlibUtils.cmake
+++ CMakeModules/ODZlibUtils.cmake
@@ -8,7 +8,7 @@
 if ( QT_QTCORE_INCLUDE_DIR )
     if ( EXISTS "${QT_QTCORE_INCLUDE_DIR}/zlib.h" )
         set ( ZLIB_INCLUDE_DIR ${QT_QTCORE_INCLUDE_DIR} )
-	set ( ZLIB_LIBRARY ${QT_QTCORE_LIBRARY_RELEASE} )
+	set ( ZLIB_LIBRARY ${QT_QTCORE_LIBRARY} )
     endif()
 endif()
 

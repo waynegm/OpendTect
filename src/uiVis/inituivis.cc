7df2d30dfc5b25a651e241afb80e03fff8314160
Ding Zheng <ding.zheng@dgbes.com>
2013-03-22 11:06:56 +0000

    added SVN property end line as native
    git-svn-id: https://svn.opendtect.org/od/branches/od_osg@28942 a985855d-d384-4b45-ae33-31a410998c3c

--- src/uiVis/inituivis.cc
+++ src/uiVis/inituivis.cc
@@ -7,7 +7,7 @@ ________________________________________________________________________
 ________________________________________________________________________
 
 -*/
-static const char* rcsID mUsedVar = "$Id: inituibase.cc 28408 2013-01-31 05:16:30Z ranojay.sen@dgbes.com $";
+static const char* rcsID mUsedVar = "$Id$";
 
 #include "uivismod.h"
 #include "moddepmgr.h"

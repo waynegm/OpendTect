4399f4abefb8043bb42c34fd76661f9958cd155a
Jaap Glas <jaap.glas@dgbes.com>
2013-04-16 13:11:59 +0000

    Replaced include file to make compile.
    git-svn-id: https://svn.opendtect.org/od/branches/od_osg@29282 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visBase/visrandomtrack.cc
+++ src/visBase/visrandomtrack.cc
@@ -24,7 +24,7 @@ static const char* rcsID mUsedVar = "$Id$";
 #include "errh.h"
 
 #include <osg/Array>
-#include <osgGeo/RandomLine>
+#include <osgGeo/TexturePanelStrip>
 
 mCreateFactoryEntry( visBase::RandomTrack );
 

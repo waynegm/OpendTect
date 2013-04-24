6bbb4aacd0a18d1e9cdfc8ee6f6e6c0594d852ae
Ding Zheng <ding.zheng@dgbes.com>
2013-04-24 09:45:32 +0000

    added id and date in svn property
    git-svn-id: https://svn.opendtect.org/od/branches/od_osg@29450 a985855d-d384-4b45-ae33-31a410998c3c

--- src/visBase/vishorizontexturehandler.cc
+++ src/visBase/vishorizontexturehandler.cc
@@ -4,7 +4,7 @@
  * DATE     : April 2013
 -*/
 
-static const char* rcsID mUsedVar = "$Id: vishorizontexturehandler.cc 28563 2013-04-16 18:05:13Z ding.zheng@dgbes.com $";
+static const char* rcsID mUsedVar = "$Id$";
 
 #include "vishorizontexturehandler.h"
 #include "vishorizonsection.h"

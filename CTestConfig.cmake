d249055e618790f2237980d4e9d0b3472d699b52
Nanne Hemstra <nanne.hemstra@dgbes.com>
2019-05-31 14:34:28 -0500

    update to 6.5

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,13 +1,15 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
 
-set(CTEST_PROJECT_NAME "OpendTect")
+set(CTEST_PROJECT_NAME "OpendTect-6_5")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "intranet")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=OpendTect")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=OpendTect-6.5")
 set(CTEST_DROP_SITE_CDASH TRUE)
-

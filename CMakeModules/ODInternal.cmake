9b9fd892a851230a92490ce7b8488b90e28373f5
Nageswara Rao <nageswara.rao@dgbes.com>
2019-06-05 11:44:04 +0530

    Not installing .cc and .h files.

--- CMakeModules/ODInternal.cmake
+++ CMakeModules/ODInternal.cmake
@@ -293,24 +293,6 @@ OD_CURRENT_DATE( DATE )
 configure_file( ${CMAKE_SOURCE_DIR}/CMakeModules/templates/buildinfo.h.in
 		${CMAKE_BINARY_DIR}/include/Basic/buildinfo.h @ONLY )
 
-#Installing source
-install( DIRECTORY ${CMAKE_SOURCE_DIR}/src ${CMAKE_SOURCE_DIR}/include
-		   ${CMAKE_SOURCE_DIR}/plugins DESTINATION ${MISC_INSTALL_PREFIX}/
-	 FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ
-	 FILES_MATCHING PATTERN "*.h" PATTERN "*.c" PATTERN "*.cc" PATTERN "*.xpm"
-			PATTERN "*.ico" PATTERN "*.rc" PATTERN "*.txt"
-			PATTERN ".svn" EXCLUDE PATTERN CMakeFiles EXCLUDE )
-
-#Installing cmake genetated files from CMAKE_BINARY_DIR directory
-if ( NOT "${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_BINARY_DIR}" )
-    install( DIRECTORY ${CMAKE_BINARY_DIR}/src ${CMAKE_BINARY_DIR}/include
-		       ${CMAKE_BINARY_DIR}/plugins DESTINATION ${MISC_INSTALL_PREFIX}/
-	     FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ
-	     FILES_MATCHING PATTERN "*.h" PATTERN "*.c" PATTERN "*.cc" PATTERN "*.xpm"
-			    PATTERN "*.ico" PATTERN "*.rc" PATTERN "*.txt"
-			    PATTERN ".svn" EXCLUDE PATTERN CMakeFiles EXCLUDE )
-endif()
-
 if ( UNIX )
     list( APPEND OD_THIRD_PARTY_LIBS ${PROJ4LIB} )
 endif()
